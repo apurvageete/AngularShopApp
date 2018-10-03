e0b3696e4092558e9cf870a640b93e2be3b25475
Admin <hemant.hemnani@consagous.com>
2018-10-03 16:28:26 +0530

    Added my project

---
+++ src/app/Model/user.ts
@@ -0,0 +1,8 @@
+export class User{
+constructor(
+    public name:string,
+    public email:string,
+    public password:string,
+    public contact_no:string,
+    public user_image:string){}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {RouterModule} from '@angular/router'
+
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database'
+import { environment } from '../environments/environment.prod';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { BgNavbarComponent } from './bg-navbar/bg-navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    BgNavbarComponent
+    BgNavbarComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([{path:'',component:HomeComponent}])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
